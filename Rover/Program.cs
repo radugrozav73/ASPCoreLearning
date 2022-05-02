b97ccbea8bc84dab9ea7cec9887734e1c2e4bd38
radugrozav73 <radugrozav73@gmail.com>
2022-05-02 15:04:47 +0300

    rover finished

---
+++ Rover/Program.cs
@@ -0,0 +1,27 @@
+﻿using static Rover.Rover;
+
+Console.SetCursorPosition(0, 0);
+Console.Write("Please enter the number of columns and rows(bigger than 3): ");
+int size;
+while (true)
+{
+    size = int.Parse(Console.ReadLine());
+
+    if (size < 3)
+    {
+        Console.WriteLine("Please enter a size bigger than 3");
+    }
+    else
+    {
+        break;
+    }
+}
+Rover.Rover rover = new Rover.Rover(size);
+
+while (true)
+{
+    ConsoleKeyInfo key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Escape) break;
+    Console.Clear();
+    rover.Move(key);
+}
---
+++ Rover/Rover.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace Rover
+{
+    internal class Rover
+    {
+        public static int UP = 24;
+        public static int DOWN = 25;
+        public static int LEFT = 17;
+        public static int RIGHT = 26;
+        private int[,] chart { get; set; }
+        public Rover(int size)
+        {
+            int direction = new Random().Next(24, 28);
+            if (direction == 27) direction = LEFT;
+            chart = new int[size, size];
+            CreateMatrix(size);
+            chart[0, 0] = SetCorrectDirection(0, 0, chart.GetLength(0) - 1, direction);
+            PrinTMatrix();
+        }
+
+        public void Move(ConsoleKeyInfo Key)
+        {
+            switch (Key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    NextMove(UP);
+                    break;
+                case ConsoleKey.DownArrow:
+                    NextMove(DOWN);
+                    break;
+                case ConsoleKey.RightArrow:
+                    NextMove(RIGHT);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    NextMove(LEFT);
+                    break;
+                default:
+                    PrinTMatrix();
+                    Console.WriteLine("Please press the correct key.");
+                    break;
+            }
+        }
+
+        private void NextMove(int directionASCII)
+        {
+            int[] arr = FindIndex(directionASCII);
+
+            if (chart[arr[0], arr[1]] != directionASCII)
+            {
+                chart[arr[0], arr[1]] = SetCorrectDirection(arr[0], arr[1], chart.GetLength(0) - 1, directionASCII);
+                PrinTMatrix();
+                return;
+            }
+            chart[arr[0], arr[1]] = 0;
+
+            if (directionASCII == UP)
+            {
+                if (arr[0] != 0) arr[0] -= 1;
+            }
+            if (directionASCII == DOWN)
+            {
+                if (arr[0] != chart.GetLength(0)) arr[0] += 1;
+            }
+            if (directionASCII == RIGHT)
+            {
+                if (arr[1] != chart.GetLength(0)) arr[1] += 1;
+            }
+            if (directionASCII == LEFT)
+            {
+                if (arr[1] != 0) arr[1] -= 1;
+            }
+            chart[arr[0], arr[1]] = SetCorrectDirection(arr[0], arr[1], chart.GetLength(0) - 1, directionASCII);
+            PrinTMatrix();
+        }
+
+        private void CreateMatrix(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    chart[i, j] = 0;
+                }
+            }
+        }
+
+        private int[] FindIndex(int directionASCII)
+        {
+            int[] indecs = new int[2];
+
+            for (int i = 0; i < chart.GetLength(0); i++)
+            {
+                for (int j = 0; j < chart.GetLength(1); j++)
+                {
+                    if (chart[i, j] == UP || chart[i, j] == DOWN || chart[i, j] == RIGHT || chart[i, j] == LEFT)
+                    {
+                        indecs[0] = i;
+                        indecs[1] = j;
+                        return indecs;
+                    }
+                }
+            }
+            return indecs;
+        }
+
+        private int SetCorrectDirection(int x, int y, int maxIndex, int direction)
+        {
+            // corners
+            if (x == 0)
+            {
+                if (y == 0 && (direction == UP || direction == LEFT))
+                {
+                    return direction == UP ? DOWN : RIGHT;
+                }
+                if (y == maxIndex && (direction == UP || direction == RIGHT))
+                {
+                    return direction == UP ? LEFT : DOWN;
+                }
+            }
+            if (x == maxIndex)
+            {
+                if (y == 0 && (direction == DOWN || direction == LEFT))
+                {
+                    return direction == DOWN ? UP : RIGHT;
+                }
+                if (y == maxIndex && (direction == DOWN || direction == RIGHT))
+                {
+                    return direction == DOWN ? UP : LEFT;
+                }
+            }
+            // at the edge but between corners
+            if (x > 0 && x < maxIndex)
+            {
+                if (y == 0 && direction == LEFT)
+                {
+                    return RIGHT;
+                }
+                if (y == maxIndex && direction == RIGHT)
+                {
+                    return LEFT;
+                }
+            }
+            if (y > 0 && y < maxIndex)
+            {
+                if (x == 0 && direction == UP)
+                {
+                    return DOWN;
+                }
+                if (x == maxIndex && direction == DOWN)
+                {
+                    return UP;
+                }
+            }
+            return direction;
+        }
+
+        private void PrinTMatrix()
+        {
+            for (int i = 0; i < chart.GetLength(0); i++)
+            {
+                for (int j = 0; j < chart.GetLength(0); j++)
+                {
+                    if (chart[i, j] == UP || chart[i, j] == DOWN || chart[i, j] == RIGHT || chart[i, j] == LEFT)
+                    {
+                        Console.Write((char)chart[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(chart[i, j] + " ");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
